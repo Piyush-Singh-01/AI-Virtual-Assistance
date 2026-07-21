2695f6bbc3b883546006fa13646bb1885b78c270
PIYUSH SINGH <piyushsinghrajput9939@gmail.com>
2026-07-21 16:01:17 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-ai-virtual-assistance.onrender.com/api",
+  baseURL: "https://ai-virtual-assistance-1.onrender.com/api",
   // baseURL: "http://localhost:8000/api",
   withCredentials: true, // if you're using cookies/sessions
 });
