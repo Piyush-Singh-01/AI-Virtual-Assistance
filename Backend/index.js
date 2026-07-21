8bf35b052d6bb4ffd33edcb4631433b105a81224
PIYUSH SINGH <piyushsinghrajput9939@gmail.com>
2026-07-21 15:50:26 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: "https://piyush-ai-virtual-assistance.onrender.com",
+    origin: "https://ai-virtual-assistance-five.vercel.app",
     // origin: "http://localhost:5173",
     credentials: true
 }));
