d107b8dcce5b638a4446c17db6ed3b766be8ca33
PIYUSH SINGH <piyushsinghrajput9939@gmail.com>
2026-07-22 01:58:41 +0530

    Update DBConnection.js

--- Backend/DBConnection.js
+++ Backend/DBConnection.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const DBConnection = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("DB connection successful");
-    } catch (error) {
-        console.log("Database connection failed",error);
-    }
-}
+const DBConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error(" Database connection failed");
+    console.error(error);
+
+    process.exit(1); // Stop the server if DB connection fails
+  }
+};
 
 module.exports = DBConnection;
